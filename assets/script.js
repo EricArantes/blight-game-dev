0ab75ddd7e68376f0de9571dced0aa567acebce3
Eric <ericarantes0@gmail.com>
2023-09-11 15:05:16 -0300

    finished layout, action log proof of concept

--- assets/script.js
+++ assets/script.js
@@ -16,5 +16,38 @@ function sidebarLeftToggle(elementId) {
     contentToShow.style.display = 'grid';
     titleToShow.style.display = 'flex';
 
+}
+
+
+
+function postThis(clickedDiv){
+  // Find the first <p> element within the clicked div
+  const paragraph = clickedDiv.querySelector('p');
+
+  if (paragraph) {
+    // Get the text content of the <p> element
+    const text = paragraph.textContent;
+
+    // Create a new <div> element
+    const outputDiv = document.createElement('div');
+
+    // Create a <p> element for the text and set its content
+    const textParagraph = document.createElement('p');
+    textParagraph.textContent = text;
+
+    // Append the <p> element to the new <div> element
+    outputDiv.appendChild(textParagraph);
+
+    // Display the new <div> element (replace "output" with the desired location)
+    const outputContainer = document.getElementById('action-log');
+    outputContainer.prepend(outputDiv);
+
+    // Check if there are more than ten children in the container
+    if (outputContainer.children.length > 10) {
+        // Remove the oldest child (the last one)
+        outputContainer.lastChild.remove();
 
   }
+}
+
+}
